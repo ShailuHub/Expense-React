b0afb405865b737d2678ed3727a8a689a6520025
Shailu <shailesh.respond@gmail.com>
2023-10-18 23:21:32 +0530

    Insert data dynamically Using Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import ExpenseItem from "./components/ExpenseItem";
-import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      date: "2023-05-01",
+      title: "Groceries",
+      amount: 50.0,
+    },
+    {
+      date: "2023-05-02",
+      title: "Dinner",
+      amount: 30.0,
+    },
+    {
+      date: "2023-05-03",
+      title: "Gasoline",
+      amount: 40.0,
+    },
+    {
+      date: "2023-06-20",
+      title: "Movie tickets",
+      amount: 25.0,
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
-      <ExpenseItem />
+      {expenses.map((item, idx) => {
+        return (
+          <ExpenseItem
+            key={idx}
+            date={item.date}
+            title={item.title}
+            amount={item.amount}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  const { date, title, amount } = props;
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Car insurence</h2>
-        <p>$105</p>
+    <div className="expense-item">
+      <div>{date}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
