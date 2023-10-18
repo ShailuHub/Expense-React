c9cdb2de7766239a2a488090f060345e69c59dbc
Shailu <shailesh.respond@gmail.com>
2023-10-18 22:02:41 +0530

    Configuring Project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Car insurence</h2>
+        <p>$105</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
